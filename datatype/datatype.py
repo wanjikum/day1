1f85ba8db7915d81493f3af86932284a5f8a67c8
wanjikum <wanjikumillicent5@gmail.com>
2017-02-07 01:29:44 +0300

    Final datatype code

--- datatype/datatype.py
+++ datatype/datatype.py
@@ -2,10 +2,25 @@
 def data_type(arg):
     """A function that returns a string based on the datatype given"""
     if isinstance(arg, str):
-    	return len(arg)
+        return len(arg)
     elif arg == None:
-    	return "no value"
+        return "no value"
     elif isinstance(arg, bool):
-    	return bool
-    	
+        return arg
+    elif isinstance(arg, int):
+        if arg < 100:
+            return 'less than 100'
+        elif arg > 100:
+            return 'more than 100'
+        else:
+            return 'equal to 100'
+    else:
+        if isinstance(arg, list):
+            if len(arg) <= 2:
+                return None
+            else:
+                return arg[2]
+
+
+
 
