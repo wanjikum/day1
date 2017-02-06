c80ab580b29fa41391576068751371f318549e45
wanjikum <wanjikumillicent5@gmail.com>
2017-02-07 01:56:44 +0300

    Working on the fizzbuzz assignment

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,11 @@
+"""A program implements the fizzbuzz app"""
+def fizz_buzz(arg):
+	 """A function that tests if the number is divisible by 3 and 5"""
+    if arg%3 == 0 and arg%5 == 0:
+    	return 'FizzBuzz'
+    elif arg%5 == 0:
+    	return 'Buzz'
+    elif arg%3 == 0:
+        return 'Fizz'
+    else:
+    	return arg
