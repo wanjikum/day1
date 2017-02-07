2691d03f4685c798a2f39e5f7d4b1a401b7866d9
wanjikum <wanjikumillicent5@gmail.com>
2017-02-07 07:22:28 +0300

    setting the number of doors

--- car/car.py
+++ car/car.py
@@ -1,12 +1,22 @@
 """Andelab Car Assignment"""
 class Car(object):
-	"""Collection of methods of class car"""
-	def __init__(self, car_type, model = "GM", car_name = "General"):
-		self.car_type = car_type
-		self.model = model
-		self.name = car_name
+    """Collection of methods of class car"""
+    def __init__(self, car_type, model = "GM", car_name = "General"):
+        self.car_type = car_type
+        self.model = model
+        self.name = car_name
 
-	if self.num_of_doors in []
+        if self.name in ['Porshe', 'Koenigsegg']:
+            self.num_of_doors = 4
+        else:
+            self.num_of_doors = 2
+
+        if self.car_type == 'trailer':
+            self.num_of_wheels = 8
+        else:
+            self.num_of_wheels = 4
+
+
 
 
 
