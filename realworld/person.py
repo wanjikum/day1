6b5aa6fbb81688f2d2f9dd8dfa8d1523d19278a1
wanjikum <wanjikumillicent5@gmail.com>
2017-02-07 12:53:57 +0300

    generating an email test fails

--- realworld/person.py
+++ realworld/person.py
@@ -4,5 +4,9 @@ polymorphism and encapsulation
 """
 class Person(object):
     """Class person implementation"""
-    def __init__(self):
-        pass
+    def __init__(self, first="Jim", last="Jam", gender="Female"):
+    	"""Class constructor"""
+    	self.first = first
+    	self.last = last
+    	self.gender = gender
+       
--- realworld/testperson.py
+++ realworld/testperson.py
@@ -11,7 +11,23 @@ class PersonClassTest(unittest.TestCase):
 	def test_person_object_type(self):
 		"""Tests if the object should be a type of `Person`"""
 		man = Person('larry')
-		self.assertIsTrue((type(man) is Person), Person)
+		self.assertTrue((type(man) is Person), Person)
+
+	def test_default_first_and_second_name(self):
+		"""Tests if the default first and last name is jim jam"""
+		woman = Person()
+		self.assertListEqual(['Jim','Jam'], [woman.first, woman.last])
+
+	def test_default_gender(self):
+		"""Tests if the default gender is female"""
+		jane = Person()
+		self.assertEqual('Female', jane.gender)
+
+	def test_objects_email(self):
+		"""Test if the persons email is generated automatically"""
+		paul = Person('Paul', 'Othis')
+		self.assertEqual('paul_othis@gmail.com', paul.email)
+		
 
 
 
