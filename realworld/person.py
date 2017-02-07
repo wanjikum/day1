fe062a91597273d191c9dc6a9d97e0efd585b28f
wanjikum <wanjikumillicent5@gmail.com>
2017-02-07 13:34:45 +0300

    Failing class persons tests

--- realworld/person.py
+++ realworld/person.py
@@ -5,8 +5,18 @@ polymorphism and encapsulation
 class Person(object):
     """Class person implementation"""
     def __init__(self, first="Jim", last="Jam", gender="Female"):
-    	"""Class constructor"""
-    	self.first = first
-    	self.last = last
-    	self.gender = gender
+        """Class constructor"""
+        self.first = first
+        self.last = last
+        self.gender = gender
+        self.email = '{}_{}@gmail.com'.format(self.first, self.last).lower()
+
+    def full_name(self):
+        """A method that returns persons fullname"""
+        pass
+
+    def is_female(self):
+    	"""A method that returns true if the persons gender is female"""
+    	pass
+
        
--- realworld/testperson.py
+++ realworld/testperson.py
@@ -2,32 +2,64 @@
 import unittest
 from person import Person
 class PersonClassTest(unittest.TestCase):
-	"""Person class tests implementations"""
-	def test_person_instance(self):
-		"""Tests if the object is an instance of class `Person`"""
-		man = Person('larry')
-		self.assertIsInstance(man, Person)
+    """Person class tests implementations"""
+    def test_person_instance(self):
+        """Tests if the object is an instance of class `Person`"""
+        man = Person('larry')
+        self.assertIsInstance(man, Person)
 
-	def test_person_object_type(self):
-		"""Tests if the object should be a type of `Person`"""
-		man = Person('larry')
-		self.assertTrue((type(man) is Person), Person)
+    def test_person_object_type(self):
+        """Tests if the object should be a type of `Person`"""
+        man = Person('larry')
+        self.assertTrue((type(man) is Person), Person)
 
-	def test_default_first_and_second_name(self):
-		"""Tests if the default first and last name is jim jam"""
-		woman = Person()
-		self.assertListEqual(['Jim','Jam'], [woman.first, woman.last])
+    def test_default_first_and_second_name(self):
+        """Tests if the default first and last name is jim jam"""
+        woman = Person()
+        self.assertListEqual(['Jim','Jam'], [woman.first, woman.last])
 
-	def test_default_gender(self):
-		"""Tests if the default gender is female"""
-		jane = Person()
-		self.assertEqual('Female', jane.gender)
+    def test_default_gender(self):
+        """Tests if the default gender is female"""
+        jane = Person()
+        self.assertEqual('Female', jane.gender)
 
-	def test_objects_email(self):
-		"""Test if the persons email is generated automatically"""
-		paul = Person('Paul', 'Othis')
-		self.assertEqual('paul_othis@gmail.com', paul.email)
-		
+    def test_objects_email(self):
+        """Test if the persons email is generated using the first and the last name"""
+        paul = Person('Paul', 'Othis')
+        self.assertEqual('paul_othis@gmail.com', paul.email)
+
+    def test_if_a_person_name_is_correct(self):
+        """Test if the person name comprised of the first name and second name"""
+        paul = Person('Paul', 'Othis')
+        self.assertEqual('I am Paul Othis', paul.full_name)
+
+    def test_if_it_outputs_correct_gender(self):
+        """Test if it returns true if persons gender is female"""
+        boot = Person('henry', 'boot', 'male')
+        self.assertTrue(boot.is_female())
+
+    def test_age_data_can_be_set(self):
+        """Test if it sets the age to 40 and accepts datatype int"""
+        fatuma = Person()
+        self.assertEqual(40, fatuma.set_age(40))
+
+    def test_age_data_can_be_set(self):
+        """Test if it sets the age to 40 and accepts datatype string"""
+        fatuma = Person()
+        self.assertEqual('forty', fatuma.set_age('forty'))
+
+    def test_if_pay_is_an_attribute(self):
+    	"""Tests if the attribut default value is 50000"""
+    	jane = Person()
+    	self.assertEqual(50000, jane.person)
+
+
+
+
+
+
+
+
 
 
 
