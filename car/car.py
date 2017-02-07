502f8f044ab45247e39c0be27dddf9a56273e4e8
wanjikum <wanjikumillicent5@gmail.com>
2017-02-07 07:05:13 +0300

    setting default values in car name and model

--- car/car.py
+++ car/car.py
@@ -1,3 +1,13 @@
 """Andelab Car Assignment"""
 class Car(object):
-	pass
+	"""Collection of methods of class car"""
+	def __init__(self, car_type, model = "GM", car_name = "General"):
+		self.car_type = car_type
+		self.model = model
+		self.name = car_name
+
+	if self.num_of_doors in []
+
+
+
+
